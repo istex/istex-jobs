3f479aba9835b66ca962c3649825d062fca838a2
parentin <jean-joffrey.parentin@inist.fr>
2022-02-28 16:05:45 +0100

    feat(config): change Config imports, now its possible to overload all the Parameters from config.json

--- src/jobBootstrapper.js
+++ src/jobBootstrapper.js
@@ -1,14 +1,14 @@
 const fs = require('fs-extra');
 const assert = require('assert').strict;
-const { app } = require('@istex/config-component').get(module);
+const { jobs } = require('@istex/config-component').get(module);
 const tasks = require('./tasks/index.js');
 const { scheduleJob } = require('./jobManager');
 
 module.exports.bootstrap = bootstrap;
 
-async function bootstrap (appConfigPath = app.configPath) {
-  return fs.readJson(appConfigPath)
-    .then(({ jobs = [] } = {}) => {
+async function bootstrap () {
+  return Promise.resolve()
+    .then(() => {
       assert.ok(Array.isArray(jobs), 'Expect jobs to be a Array');
       if (!jobs || !jobs.length) return;
 
